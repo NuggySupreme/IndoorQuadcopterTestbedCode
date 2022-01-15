1bc5ec3c7de44886b503f9e16828bf142b13281c
BuildTools <unconfigured@null.spigotmc.org>
2022-01-14 20:12:10 -0600

    Adding comments to constaints and functions. -CJG

--- PowerSupplyControls/PowerSupplyControls.ino
+++ PowerSupplyControls/PowerSupplyControls.ino
@@ -7,16 +7,21 @@ byte powerSupplyAddresses[] = {
   B1010011
 };
 
+// OFF and ON constents for power supply. 
+// 8 bits, first bit for off(1) or on(0), last bit
+// must be 1 to 
 byte OFF = B10000001;
 byte ON = B00000001;
+// Register address for power supply
 byte Power_Register = 0x7C; // Power Control Register
 
+// runs on init startup
 void setup() {
   Wire.begin(); // Initiate the Wire library
   Serial.begin(9600);
   delay(100);
-  // Enable measurement
-  for ( int i = 0; i < 4; i++) {
+  // Startup sequence: turns each bank on one at a time, then off.
+  for (int i = 0; i < 4; i++) {
     turnOn(powerSupplyAddresses[i]);
     delay(1000);
   }
@@ -26,6 +31,7 @@ void setup() {
   }
 }
 
+// runs continuously after setup
 void loop() {
   Serial.println("Which bank would you like to target? Please enter 0, 1, 2, 3.");
   while(!Serial.available()) {
@@ -33,7 +39,7 @@ void loop() {
   }
   int bank = Serial.parseInt();
   Serial.read();
-  
+
   Serial.println("Would you like to turn this bank on or off? Please enter 0 for on and 1 for off");
   while(!Serial.available()) {
     
@@ -47,20 +53,18 @@ void loop() {
    }
 }
 
+// turns off power supply at given address
 void turnOff(byte powerSupply) {
   Wire.beginTransmission(powerSupply);
-
   Wire.write(Power_Register);
-  // Bit D3 High for measuring enable (0000 1000)
   Wire.write(OFF);
   Wire.endTransmission();
 }
 
+// turns on power supply at given address
 void turnOn(byte powerSupply) {
   Wire.beginTransmission(powerSupply);
-
   Wire.write(Power_Register);
-  // Bit D3 High for measuring enable (0000 1000)
   Wire.write(ON);
   Wire.endTransmission();
 }
